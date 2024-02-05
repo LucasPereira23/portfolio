9726bc099d428e2ca061c253ddebb5925b56701d
Lucas Pereira <chaes_231200@hotmail.com>
2024-02-05 12:06:41 -0300

    tweak/ muda tags no header de link para span

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -43,7 +43,7 @@ function Header() {
         <a href="https://lucaspereira.vercel.app/" className="border-2 px-4 py-2 text-white-100 rounded-md hover:bg-white-100/10 transition-all duration-300">L</a>
         <ul className="m-0 p-0 sm-mobile:hidden mobile:hidden sm:hidden md:flex items-center justify-center">
           <li className="list-none text-white-100 py-0 px-5 text-sm font-light ">
-            <a href="" className="mr-2 text-red-500 font-normal">01.</a>
+            <span className="link-font mr-2 text-red-500 font-normal">01.</span>
             <Link
               activeClass="active"
               to="content-about"
@@ -56,7 +56,7 @@ function Header() {
             </Link>
           </li>
           <li className="list-none text-white-100 py-0 px-5 text-sm font-light ">
-            <a href="" className="mr-2 text-red-500 font-normal">02.</a>
+            <span className="link-font mr-2 text-red-500 font-normal">02.</span>
             <Link
               activeClass="active"
               to="content-work"
@@ -68,7 +68,7 @@ function Header() {
             </Link>
           </li>
           <li className="list-none text-white-100 py-0 px-5 text-sm font-light ">
-            <a href="" className="mr-2 text-red-500 font-normal">03.</a>
+            <span className="link-font mr-2 text-red-500 font-normal">03.</span>
             <Link
               activeClass="active"
               to="content-contact"
